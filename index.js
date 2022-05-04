f8b23aeb3cc1ef565f61864261d4cd59c27cf2f5
Nicolas Espel <nicolasespelcoding@gmail.com>
2022-05-04 13:08:12 -0400

    Added animations to hamburger button and media queries

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const hamburger = document.getElementById('hamburger');
 const overlay = document.querySelector('.overlay');
 const overlayMenu = document.querySelector('.overlay-menu');
+const toggle = document.querySelector('.burger-toggle');
 
 hamburger.addEventListener('click', () => {
     if (hamburger.classList.contains('.open')) {
         hamburger.classList.remove('.open');
+        hamburger.classList.remove('hamburger-toggle');
         overlay.style.display = 'none';
-        overlayMenu.style.display = 'none'
+        overlayMenu.style.display = 'none';
     } else {
         hamburger.classList.add('.open');
+        hamburger.classList.add('hamburger-toggle');
         overlay.style.display = 'block';
         overlayMenu.style.display = 'block'
 
